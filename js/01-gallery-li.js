0e0b91809c973a523d81a31c60a11473ba90afc2
denisov-vladimir <denisoff@i.ua>
2023-11-05 23:56:57 +0200

    hw7js
    hw7js

--- js/01-gallery-li.js
+++ js/01-gallery-li.js
@@ -8,11 +8,11 @@ let makeLi = '';
 
 galleryItems.forEach((galleryItem) => {
     const itemLi = `<li class="gallery__item">
-                        <a class="gallery__link" href="${galleryItem.original}">
-                        <img class="gallery__image lazyload"
-                            src="${galleryItem.preview}" 
-                            data-source="${galleryItem.original}"
-                            alt="${galleryItem.description}" /> </a> </li>`;
+        <img
+            class="gallery__image lazyload"
+            src="${galleryItem.preview}" 
+            data-source="${galleryItem.original}"
+            alt="${galleryItem.description}" > </li> `;
     
     makeLi += itemLi;
 }); 
@@ -25,22 +25,22 @@ const imgModal = document.querySelector('.gallery__link');
 ulGallery.addEventListener('click', openModal);
 
 function openModal(event) {
-    event.preventDefault();
     if (event.target.nodeName === "UL") {      return;    }
-
+ 
     if (event.target.nodeName === "IMG") {
-        butCloseBtn.nextElementSibling.src = event.target.dataset.source; 
+        imgModal.src = event.target.dataset.source;
+        imgModal.alt = event.target.alt;
+        divBackdrop.classList.add(`open`);
+        window.addEventListener('keydown', closeByEsc);
+        divBackdrop.addEventListener('click', closeByBackdrop);
+        butCloseBtn.addEventListener('click', closeModal);  
     }
-
-    divBackdrop.classList.add(`open`);
-    window.addEventListener('keydown', closeByEsc);
-    divBackdrop.addEventListener('click', closeByBackdrop);
-    butCloseBtn.addEventListener('click', closeModal);  
-   }
+}
 
 function closeModal() {
     divBackdrop.classList.remove(`open`);
-    butCloseBtn.nextElementSibling.src = '';
+    imgModal.src = '';
+    imgModal.alt = '';
     window.removeEventListener('keydown', closeByEsc);
 }
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const ulGallery = document.querySelector('.gallery');
+
+let makeLi = '';
+
+galleryItems.forEach((galleryItem) => {
+    const itemLi = `<li class="gallery__item">
+                        <a class="gallery__link" href="${galleryItem.original}">
+                        <img class="gallery__image"
+                            src="${galleryItem.preview}" 
+                            alt="${galleryItem.description}" /> </a> </li>`;
+    
+    makeLi += itemLi;
+}); 
+
+ulGallery.insertAdjacentHTML('beforeend', makeLi);   
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    });
